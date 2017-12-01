56c56da303f658b52749984381016fda502823cd
Gleibson Alves <alvesgleibson@gmail.com>
2017-12-01 00:40:31 -0300

    subindo a segunda Activity

---
+++ AndroidStudioProjects/Messagem/app/src/main/java/br/com/ondanetpe/messagem/ReceiveMessageActivity.java
@@ -0,0 +1,13 @@
+package br.com.ondanetpe.messagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+    }
+}
