48f6d7b1208f926e2c2e93e3aad7d22c6e45c552
Gleibson Alves <alvesgleibson@gmail.com>
2017-12-01 00:43:33 -0300

    Subindo o restante dos arquivos

---
+++ AndroidStudioProjects/Messagem/app/src/main/java/br/com/ondanetpe/messagem/CreateMessageActicity.java
@@ -0,0 +1,23 @@
+package br.com.ondanetpe.messagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateMessageActicity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view){
+
+
+
+
+
+
+    }
+}
