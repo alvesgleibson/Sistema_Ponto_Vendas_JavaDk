afec5e6d7e2df4d5c6cd9d2e6017f997eb8155a6
Gleibson Alves <alvesgleibson@gmail.com>
2017-12-01 19:47:00 -0300

    So

--- AndroidStudioProjects/Messagem/app/src/main/java/br/com/ondanetpe/messagem/CreateMessageActicity.java
+++ AndroidStudioProjects/Messagem/app/src/main/java/br/com/ondanetpe/messagem/CreateMessageActicity.java
@@ -1,10 +1,14 @@
 package br.com.ondanetpe.messagem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
 
 public class CreateMessageActicity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +18,12 @@ public class CreateMessageActicity extends AppCompatActivity {
 
     public void onSendMessage(View view){
 
+        EditText mensagenEnviar = (EditText)findViewById(R.id.message);
+        String mens = mensagenEnviar.getText().toString();
 
-
+       Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra("message",mens);
+        startActivity(intent);
 
 
 
