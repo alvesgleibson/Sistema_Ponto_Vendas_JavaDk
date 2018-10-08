b4642705a1c074a7fa6c3e0e3bca6eb651a7c8e3
GleibsonAlves <alvesgleibso@gmail.com>
2018-10-08 15:14:28 -0300

    Iniciando o projeto Ponto de Vendas no Netbeans (Canada my dream).

--- Documents/NetBeansProjects/Sistema/src/sistema/Sistema.java
+++ Documents/NetBeansProjects/Sistema/src/sistema/Sistema.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sistema;
 
-/**
- *
- * @author Gleibson Alves
- */
 public class Sistema {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Testeando O Git for Cana");
     }
     
 }
