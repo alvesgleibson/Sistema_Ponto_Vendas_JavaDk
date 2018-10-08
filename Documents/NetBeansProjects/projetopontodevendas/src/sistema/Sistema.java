a60bf64556985445c03cddc14a5be72cbd8c3c7d
GleibsonAlves <alvesgleibso@gmail.com>
2018-10-08 15:45:11 -0300

    Iniciando o projeto Ponto de Vendas no Netbeans (Canada my dream).

--- Documents/NetBeansProjects/projetopontodevendas/src/sistema/Sistema.java
+++ Documents/NetBeansProjects/projetopontodevendas/src/sistema/Sistema.java
@@ -6,6 +6,7 @@ public class Sistema {
     
     public static void main(String[] args) {
         System.out.println("Testeando O Git for Canada");
+        System.out.println("Indo Para o canda no meio do ano que vem");
     }
     
 }
